7027a5267ddc55cca80deed84147df56403ece11
yohei <yangpinggaoye15@gmail.com>
2026-03-22 20:37:29 +0900

    fix: use kv-asset-handler for static file serving

--- worker.js
+++ worker.js
@@ -1,5 +1,11 @@
-﻿export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+﻿import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      return await getAssetFromKV({ request, waitUntil: ctx.waitUntil.bind(ctx) }, { ASSET_NAMESPACE: env.__STATIC_CONTENT, ASSET_MANIFEST: env.__STATIC_CONTENT_MANIFEST });
+    } catch (e) {
+      return new Response("Not found", { status: 404 });
+    }
   }
 };
